f937fd42e10a64121f50d427142e213d52793119
AniJessica21CN029563 <anijessica6896@gmail.com>
2024-04-07 23:46:56 +0100

    Assinment 2
    Submission

---
+++ Solution for Assignment 2.py
@@ -0,0 +1,10 @@
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/Ani Jessica/Documents/Python 2024/My Documents/Assignment 2.py
+What is the prosity of the reservoir0.25
+What is the area of the reservoir30
+What is the thickness of the reservoir2
+What is the water saturation of the reservoir0.5
+What is the formaton volume factor of the reservoir35
+The amount of oil initially in place is 1662.4285714285713
